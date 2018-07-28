ded6ac3b9ee565fed65afb4a11bd3089a24f56a1
Idan Hakim <idanhakim123@gmail.com>
2018-07-28 19:57:58 +0300

    create

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,22 +6,32 @@ const userService = require('../services/userService')
 
 module.exports = (app) => {
 
-    app.get('/users/', (req, res) => {
+    app.get(`${USER_URL}/`, (req, res) => {
         // const userId = req.params.userId
         // console.log('userRoute', userId)
         return userService.query()
             .then(users => {
                 res.json(users)
             })
-            .catch(err => {console.log('some problem')})
+            .catch(err => {
+                console.log('some problem')
+            })
     })
-        
-    app.post('/users/login', (req, res) => {
+
+    app.post(`${USER_URL}/login`, (req, res) => {
         userService.checkForUser(req.body)
             .then(user => {
                 req.session.loggedinUser = user
                 res.json(user)
             })
-            .catch(err =>  {console.log('Wrong username or password')})
+            .catch(err => {
+                console.log('Wrong username or password')
+            })
+    })
+
+    app.put(`${USER_URL}/:userId/addCustomer`, (req, res) => {
+        userService.addCustomer(req.params.userId, req.body)
+            .then(res => console.log('Customer Add!'))
+            .catch(err => console.log('Customer not Add!'))
     })
 }
