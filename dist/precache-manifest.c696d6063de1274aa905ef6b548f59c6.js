b1c42c146ff1da86ee5fefb456f76d5adc36923b
galyo9 <galyogev5@gmail.com>
2018-08-01 19:55:43 +0300

    upload to heroku

---
+++ dist/precache-manifest.c696d6063de1274aa905ef6b548f59c6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "76bc045e152c3ca272a4a3481c685101",
+    "url": "/img/carosel1.png"
+  },
+  {
+    "revision": "eb3d7a547dec7de40f8cdab2334cb8dd",
+    "url": "/img/background.jpg"
+  },
+  {
+    "revision": "3417194cfcd51a31bddfbd427212f78f",
+    "url": "/img/carosel4.png"
+  },
+  {
+    "revision": "72a7b8073d66338daf03",
+    "url": "/js/chunk-vendors.72a7b807.js"
+  },
+  {
+    "revision": "e6ec0590a06e7ea4903d960a8595e085",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73fb0af891082a2c404c73c99b22633",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "69b76b69daa643f8c082",
+    "url": "/js/app.69b76b69.js"
+  },
+  {
+    "revision": "e10b04f06cf0ba8698d0f34b1e4882c5",
+    "url": "/img/carosel2.png"
+  },
+  {
+    "revision": "319c6725542f78913de7ee706dfc4fc4",
+    "url": "/img/carosel3.png"
+  },
+  {
+    "revision": "aabb0727f79dfca67e2ac987768eaa94",
+    "url": "/img/homeImg.jpeg"
+  },
+  {
+    "revision": "72a7b8073d66338daf03",
+    "url": "/css/chunk-vendors.0cdb0f03.css"
+  },
+  {
+    "revision": "69b76b69daa643f8c082",
+    "url": "/css/app.039c345c.css"
+  }
+];
